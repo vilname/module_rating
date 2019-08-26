a733dabbb8eefa4afa6801d959d58e6605a6948c
Alexander <test@test.ru>
2019-08-26 20:40:31 +0500

    Доделал пагинация в админке для модуля

--- install/components/rating/showRatingAdmin/templates/.default/script.js
+++ install/components/rating/showRatingAdmin/templates/.default/script.js
@@ -2,19 +2,24 @@ $(document).ready(function() {
 
     $('.admin_rating__button-js').on('click', function(e){
         var order_id = $('.admin-rating__input-js').val() ? $('.admin-rating__input-js').val() : ''
-        $.ajax({
-            url: '',
-            // dataType: 'json',
-            data: {
-                order_id,
-                action: 'search-order'
-            },
-            success: function(html){
-                $('.admin-rating__container-js').empty()
-                $('.admin-rating__container-js').append(html)
-                // console.log(html)
-            }
-        });
+
+        if(!order_id){
+            location.reload()
+        }else{
+            $.ajax({
+                url: '',
+                // dataType: 'json',
+                data: {
+                    order_id,
+                    action: 'search-order'
+                },
+                success: function(html){
+                    $('.admin-rating__container-js').empty()
+                    $('.admin-rating__container-js').append(html)
+                    // console.log(html)
+                }
+            });
+        }
     })
 
 });
